0bb2517bc6b0fd9db1b3ddbc82de3e18d035e419
Pratik Dey <56970651+PratikDey@users.noreply.github.com>
2021-02-10 13:49:50 +0530

    Add files via upload

--- web/pages/index.tsx
+++ web/pages/index.tsx
@@ -1,7 +1,20 @@
-import Head from "next/head";
 import styles from "../styles/Home.module.css";
 import Link from "next/link";
-// import { getPosts } from "./api/posts";
+import Image from "next/image";
+import { FaFacebookF, FaLinkedinIn } from "react-icons/fa";
+import "bootstrap/dist/css/bootstrap.min.css";
+import {
+  Card,
+  CardImg,
+  CardBody,
+  CardTitle,
+  CardSubtitle,
+  Button,
+  CardDeck,
+  Row,
+  Container,
+  Col,
+} from "reactstrap";
 
 const CONTENT_API_KEY = "1ec9ee62fb56a8f2258e4e2573";
 const BLOG_URL = "http://localhost:2368";
@@ -9,16 +22,23 @@ const BLOG_URL = "http://localhost:2368";
 type Post = {
   title: string;
   slug: string;
+  excerpt: string;
+  reading_time: Number;
+  feature_image: string;
+  authors: [string];
+  name: string;
 };
 
 async function getPosts() {
   const res = await fetch(
-    `${BLOG_URL}/ghost/api/v3/content/posts/?key=${CONTENT_API_KEY}&fields=title,slug,excerpt,reading_time`
+    `${BLOG_URL}/ghost/api/v3/content/posts/?key=${CONTENT_API_KEY}&include=authors`
   ).then((res) => res.json());
+
+  console.log(res.posts[0].authors);
 
   const posts = res.posts;
 
-  console.log(posts);
+  // console.log(posts.title);
 
   return posts;
 }
@@ -32,17 +52,115 @@ export const getStaticProps = async ({ params }) => {
 
 const Home: React.FC<{ posts: Post[] }> = (props) => {
   return (
-    <div className={styles.container}>
-      <h1>HEllo :)</h1>
-      {props.posts.map((post, index) => {
-        return (
-          <li key={index}>
-            <Link href="post/[slug]" as={`post/${post.slug}`}>
-              <a>{post.title}</a>
+    <div style={{ backgroundColor: "#15171a" }}>
+      <header>
+        <nav className={styles.navbar}>
+          <img
+            className={styles.ABlogo}
+            src="https://media-exp1.licdn.com/dms/image/C560BAQFvseEkqmA7nw/company-logo_200_200/0/1608369027923?e=2159024400&v=beta&t=R7QXw4D-3MvoXsSq-_Avcnle9mlFDFNGnUC0LlqMnSk"
+            alt="Artificial Brix"
+          />
+          <span className={styles.txt_ctr}>
+            <span>categories</span>
+            <Link href="/">
+              <span>mainPage</span>
             </Link>
-          </li>
-        );
-      })}
+          </span>
+          <span className={styles.nav_icons}>
+            <Link href="https://www.facebook.com/artificialbrix">
+              <FaFacebookF className={styles.ico} />
+            </Link>
+            <Link href="https://in.linkedin.com/company/artificial-brix">
+              <FaLinkedinIn className={styles.ico} />
+            </Link>
+          </span>
+        </nav>
+      </header>
+      <div className={styles.container}>
+        <Container>
+          <Row>
+            {props.posts.map((post, index) => {
+              return (
+                <Col key={index} sm={4}>
+                  <div>
+                    <CardDeck className="mb-4">
+                      <Card
+                        style={{
+                          minHeight: "380px",
+                          backgroundColor: "#15171a",
+                        }}
+                      >
+                        <CardImg
+                          top
+                          width="100%"
+                          src={post.feature_image}
+                          style={{
+                            backgroundSize: "cover",
+                          }}
+                        />
+                        <CardBody>
+                          <Link href="post/[slug]" as={`post/${post.slug}`}>
+                            <a>
+                              <CardTitle
+                                style={{
+                                  fontSize: "25px",
+                                  fontWeight: 600,
+                                  color: "hsla(0,0%,100%,.85)",
+                                }}
+                              >
+                                {post.title}
+                              </CardTitle>
+                            </a>
+                          </Link>
+                          <CardSubtitle style={{ color: "#90a2aa" }}>
+                            {post.excerpt}
+                          </CardSubtitle>
+                          {post.authors.map((author) => (
+                            <CardSubtitle
+                              key={author.id}
+                              style={{
+                                color: "hsla(0,0%,100%,.85)",
+                                padding: "20px 0",
+                              }}
+                            >
+                              Author -{" "}
+                              <span style={{ fontWeight: 600 }}>
+                                {author.name}
+                              </span>
+                            </CardSubtitle>
+                          ))}
+                          {/* <Link href="post/[slug]" as={`post/${post.slug}`}>
+                            <a>
+                              <Button color="primary">View full blog</Button>
+                            </a>
+                          </Link> */}
+                        </CardBody>
+                      </Card>
+                    </CardDeck>
+                  </div>
+                </Col>
+              );
+            })}
+          </Row>
+        </Container>
+      </div>
+      <div className={styles.footer}>
+        <Link href="https://www.artificialbrix.com/">
+          ©copyright Artificial Brix
+        </Link>
+      </div>
+      <style jsx global>
+        {`
+          .read-container {
+            max-width: 600px;
+            margin: 0 auto;
+          }
+          img {
+            max-width: 100%;
+            height: auto;
+          }
+        `}
+      </style>
     </div>
   );
 };
