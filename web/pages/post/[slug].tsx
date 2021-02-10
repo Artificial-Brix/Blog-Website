0f878a7fd7ea580bc439ffaa2cbc0843ee430c3a
Pratik Dey <56970651+PratikDey@users.noreply.github.com>
2021-02-10 13:51:23 +0530

    Add files via upload

--- web/pages/post/[slug].tsx
+++ web/pages/post/[slug].tsx
@@ -1,15 +1,18 @@
 import Link from "next/link";
 import { useRouter } from "next/router";
+import styles from "../styles/Home.module.css";
+import { FaFacebookF, FaLinkedinIn } from "react-icons/fa";
 
 const CONTENT_API_KEY = "1ec9ee62fb56a8f2258e4e2573";
 const BLOG_URL = "http://localhost:2368";
 
 async function getPost(slug: string) {
   const res = await fetch(
-    `${BLOG_URL}/ghost/api/v3/content/posts/slug/${slug}?key=${CONTENT_API_KEY}&fields=title,slug,html`
+    `${BLOG_URL}/ghost/api/v3/content/posts/slug/${slug}?key=${CONTENT_API_KEY}&include=authors`
   ).then((res) => res.json());
 
   const posts = res.posts;
+  // console.log(posts.authors);
 
   return posts[0];
 }
@@ -26,6 +29,7 @@ type Post = {
   title: string;
   html: string;
   slug: string;
+  authors: [string];
 };
 
 export const getStaticPaths = () => {
@@ -43,14 +47,50 @@ const Post: React.FC<{ post: Post }> = (props) => {
     return <h1>Loading...</h1>;
   }
 
-  console.log(props);
+  // console.log(post.authors);
   return (
-    <div>
-      <Link href="/">
-        <a>Go back simon</a>
-      </Link>
-      <h1>hello</h1>
-      <div dangerouslySetInnerHTML={{ __html: post.html }}></div>
+    <div style={{ backgroundColor: "#15171a" }}>
+      <header>
+        <nav className={styles.navbar}>
+          <img
+            className={styles.ABlogo}
+            src="https://media-exp1.licdn.com/dms/image/C560BAQFvseEkqmA7nw/company-logo_200_200/0/1608369027923?e=2159024400&v=beta&t=R7QXw4D-3MvoXsSq-_Avcnle9mlFDFNGnUC0LlqMnSk"
+            alt="Artificial Brix"
+          />
+          <span className={styles.txt_ctr}>
+            <span>categories</span>
+            <Link href="/">
+              <span>mainPage</span>
+            </Link>
+          </span>
+          <span className={styles.nav_icons}>
+            <Link href="https://www.facebook.com/artificialbrix">
+              <FaFacebookF className={styles.ico} />
+            </Link>
+            <Link href="https://in.linkedin.com/company/artificial-brix">
+              <FaLinkedinIn className={styles.ico} />
+            </Link>
+          </span>
+        </nav>
+      </header>
+      <div className="read-container">
+        <Link href="/">
+          <a>Go back simon</a>
+        </Link>
+        <div dangerouslySetInnerHTML={{ __html: post.html }}></div>
+        <style jsx global>
+          {`
+            .read-container {
+              max-width: 600px;
+              margin: 0 auto;
+            }
+            img {
+              max-width: 100%;
+              height: auto;
+            }
+          `}
+        </style>
+      </div>
     </div>
   );
 };
